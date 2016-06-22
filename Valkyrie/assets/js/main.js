2b36197fd1017e3206c190e236ad8c49c5e270d1
Amit Palekar <amit12.palekar@gmail.com>
2016-06-22 00:22:20 -0700

    added drop-down menu, blog reference, 2 members and new pages

--- Valkyrie/assets/js/main.js
+++ Valkyrie/assets/js/main.js
@@ -1,9 +1,12 @@
 $('document').ready(function() {
-  $('.menu-item').css('font-style', 'italic');
-  $('.menu-item').hover(function() {
-    $(this).css('font-size', '24px');
-    $(this).css('font-style', 'normal');
-  }, function() {
-    $(this).css('font-size', '16px');
+  var subMenuIsOpen = false;
+  $('.caret').click(function(event) {
+    if(subMenuIsOpen) {
+      subMenuIsOpen = false;
+
+    } else {
+      subMenuIsOpen = true;
+    }
+
   });
 });
